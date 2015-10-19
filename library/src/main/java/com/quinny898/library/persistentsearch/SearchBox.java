49436f44f59609c897d915f95be66c5a00e0a148
Frederic Ye <yefrederic@gmail.com>
2015-10-19 19:24:10 +0200

    don't dispatch key event

--- library/src/main/java/com/quinny898/library/persistentsearch/SearchBox.java
+++ library/src/main/java/com/quinny898/library/persistentsearch/SearchBox.java
@@ -929,16 +929,15 @@ public class SearchBox extends RelativeLayout {
 		}
 	}
 
-    @Override
-    public boolean dispatchKeyEvent(KeyEvent e) {
-        if(e.getKeyCode() == KeyEvent.KEYCODE_BACK && getVisibility() == View.VISIBLE){
-            hideCircularly((Activity) getContext());
-            return true;
-        }
-
-        return super.dispatchKeyEvent(e);
-    }
-
+//    @Override
+//    public boolean dispatchKeyEvent(KeyEvent e) {
+//        if(e.getKeyCode() == KeyEvent.KEYCODE_BACK && getVisibility() == View.VISIBLE){
+//            hideCircularly((Activity) getContext());
+//            return true;
+//        }
+//
+//        return super.dispatchKeyEvent(e);
+//    }
 
     public interface SearchListener {
 		/**
